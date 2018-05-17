f2467486c9e3cde37d09b6b197e34a944306e31d
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2018-05-17 03:38:35 +0300

    Add files via upload

---
+++ tests/SMP_test/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+int main() {
+	SMP_test Test1;
+	Test1.Foucalt_default_pen_t_1_01695_561e_06();
+	Test1.Foucalt_x0_1_v0_0_5_m_0_1_l_15_t_1();
+	Test1.Foucalt_x0_0_v0_2_m_0_1_l_15_t_2();
+	Test1.x_x0_0_v0_2_m_0_1_l_15_t_2();
+}
