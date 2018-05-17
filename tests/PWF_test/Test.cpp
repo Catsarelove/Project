ff69d9277c897f306b711625125f16f6c7b1a5c5
Catsarelove <32133612+Catsarelove@users.noreply.github.com>
2018-05-17 04:09:09 +0300

    Add files via upload

---
+++ tests/PWF_test/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+int main() {
+	PWF_test Test1;
+	Test1.x1_x0_0_2_w0_0_05_v0_0_2_c_0_0002_t_1();
+	Test1.x2_x0_0_2_w0_0_07_v0_2_c_0_2_t_6();
+	Test1.x3_x0_0_2_w0_1_v0_2_c_1_t_23();
+	return 0;
+}
